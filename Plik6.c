00e5e21e9966e21b9fb6c66b06bf88cf8258a36a
marganski <domestos234@wp.pl>
2017-11-15 19:37:57 +0100

    Zmiana w branchu 1,6

--- Plik6.c
+++ Plik6.c
@@ -1,5 +1,5 @@
 // Scalanie dwóch posortowanych list jednokierunkowych
-// Data: 25.02.2013
+// Data: 26.02.2013
 // (C)2012 mgr Jerzy Wa³aszek
 //----------------------------------------------------
 
